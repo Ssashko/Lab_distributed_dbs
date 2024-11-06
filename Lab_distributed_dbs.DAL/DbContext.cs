1d2138876514dcf86edf6f79ca8d7eda958de065
Oleksandr Marianchuk <marianchuk.oleksandr@chnu.edu.ua>
2024-11-06 13:34:35 +0200

    DAL project issue fix

--- Lab_distributed_dbs.DAL/DbContext.cs
+++ Lab_distributed_dbs.DAL/DbContext.cs
@@ -7,23 +7,38 @@ namespace Lab_distributed_dbs.DAL
         public LabDbContext(DbContextOptions<LabDbContext> options) : base(options)
         {
         }
+        public DbSet<Group> Groups { get; set; }
         public DbSet<Student> Students { get; set; }
+        public DbSet<Lecturer> Lecturers { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<StudentCourse> StudentCourses { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<StudentCourse>()
-            .HasKey(sc => new { sc.StudentId, sc.CourseId });
+            modelBuilder.Entity<Student>()
+                .HasOne(s => s.Group)
+                .WithMany(g => g.Students)
+                .HasForeignKey(s => s.GroupName);
+
+            modelBuilder.Entity<Course>()
+                .HasOne(c => c.Lecturer)
+                .WithMany(l => l.Courses)
+                .HasForeignKey(c => c.LecturerId);
 
             modelBuilder.Entity<StudentCourse>()
-            .HasOne(sc => sc.Student)
-            .WithMany(s => s.StudentCourses)
-            .HasForeignKey(sc => sc.StudentId);
+                .HasKey(sc => new { sc.StudentId, sc.CourseId });
+            modelBuilder.Entity<Group>()
+                .HasKey(g => g.Name);
 
             modelBuilder.Entity<StudentCourse>()
-            .HasOne(sc => sc.Course)
-            .WithMany(c => c.StudentCourses)
-            .HasForeignKey(sc => sc.CourseId);
+                .HasOne(sc => sc.Student)
+                .WithMany(s => s.StudentCourses)
+                .HasForeignKey(sc => sc.StudentId);
+
+            modelBuilder.Entity<StudentCourse>()
+                .HasOne(sc => sc.Course)
+                .WithMany(c => c.StudentCourses)
+                .HasForeignKey(sc => sc.CourseId);
         }
+
     }
 }
--- Lab_distributed_dbs.DAL/StudentModels.cs
+++ Lab_distributed_dbs.DAL/StudentModels.cs
@@ -1,43 +1,63 @@
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Lab_distributed_dbs.DAL
 {
 
     public class Group
-    { 
+    {
+        [BsonId]
+        [BsonElement("_id")]
         public string Name { get; set; }
         public string Specialization { get; set; }
+
+        [BsonIgnore]
         public ICollection<Student> Students { get; set; }
     }
 
     public class Student
     {
+        [BsonId]
+        [BsonElement("_id")]
         public int StudentId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string GroupName { get; set; }
+        public Group Group { get; set; }
         public ICollection<StudentCourse> StudentCourses { get; set; }
     }
 
     public class Lecturer
     {
+        [BsonId]
+        [BsonElement("_id")]
         public int LecturerId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
 
     }
 
     public class Course
     {
+        [BsonId]
+        [BsonElement("_id")]
         public int CourseId { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
         public int LecturerId { get; set; }
+        [BsonIgnore]
         public Lecturer Lecturer { get; set; }
+        [BsonIgnore]
         public ICollection<StudentCourse> StudentCourses { get; set; }
     }
 
     public class StudentCourse
     {
+        [BsonId]
+        [BsonElement("_id")]
         public int StudentId { get; set; }
+        [BsonIgnore]
         public Student Student { get; set; }
         public int CourseId { get; set; }
         public Course Course { get; set; }
